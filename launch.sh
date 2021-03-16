b3618e650aec659d5802c0bf5de163181aca4aaa
Taylor Beeston <beeston.taylor@gmail.com>
2021-03-16 11:04:23 -0700

    :art: 3/16/21 Update

--- launch.sh
+++ launch.sh
@@ -9,10 +9,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config locations ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload example -c ~/.config/polybar.old/config &
   done
 else
-  polybar --reload example &
+  polybar --reload example -c ~/.config/polybar.old/config &
 fi
 
 echo "Polybar launched..."
